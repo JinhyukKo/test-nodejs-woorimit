64327eba32db83b6e8c0f08343465627c6927ec8
loke <a1@1ui-MacBook-Pro.local>
2023-04-24 04:28:03 +0900

    15

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,2 +1,16 @@
 'use strict'
-console.log('hello');
+console.log('h');
+
+const id = document.querySelector('#id');
+const pw = document.querySelector('#password');
+const loginbtn = document.querySelector('#login-button');
+
+console.log(id, pw, loginbtn);
+loginbtn.addEventListener("click",login);
+function login(){
+    const req = {
+        id : id.value,
+        pw : pw.value,
+    };
+    console.log(req);
+}
