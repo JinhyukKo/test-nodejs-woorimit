f79f452c44dba2edc1c8ac34eff906a660433da6
loke <a1@1ui-MacBook-Pro.local>
2023-04-24 03:46:21 +0900

    12

---
+++ app/app.js
@@ -0,0 +1,24 @@
+'use strict'
+const express=require('express');
+const app = express();
+const PORT =3000;
+
+//routing
+const router = require('./src/routes/home/index')
+
+
+//setting
+app.set('views','./src/views');
+app.set('view engine','ejs')
+
+
+
+//middleware
+app.use('/',router);
+
+
+//listen
+app.listen(PORT,()=>{
+    
+    console.log('server on');
+})
