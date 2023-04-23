f17c3aaa4d2f9ffc8da14d7852d8013a357c31b4
loke <a1@1ui-MacBook-Pro.local>
2023-04-24 04:34:39 +0900

    16

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -12,5 +12,11 @@ function login(){
         id : id.value,
         pw : pw.value,
     };
-    console.log(req);
+    fetch("/login",{
+        method : "POST",
+        header: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(req)
+    });
 }
